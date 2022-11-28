b522975c01d880e085e1c17b06f77830879da762
anvar-muzaffarli <prof.anvar2013@gmail.com>
2022-11-28 21:53:24 +0400

    bu kodlar veb proqramlashdirmasinin nuvesi hesab olunur

---
+++ script.js
@@ -0,0 +1,87 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const whatsappBtn = document.getElementById('btn')
+
+// const cancelBtn = document.getElementById('cancelbtn')
+
+// const myPopUp = document.getElementById('form')
+
+// whatsappBtn.addEventListener('click', qutunuGoster)
+// cancelBtn.addEventListener('click', qutunuBagla)
+
+
+// function qutunuGoster() {
+//     myPopUp.classList.add('active')
+// }
+
+// function qutunuBagla(e) {
+// e.preventDefault()
+
+// myPopUp.classList.remove('active')
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // // createElement - mpvcud olmayan elementler ucun
+// // // getElementById, querySelector, getElementsByClassName
+
+// // // alert("sdfs")
+
+// // const accordionBtns = document.getElementsByClassName('accordion')
+// // console.log(accordionBtns)
+
+
+// // // BIR NOMRELI SUAL - THIS
+// // for(let i=0; i< accordionBtns.length; i++) {
+// //     accordionBtns[i].addEventListener("click", function() {
+// //         let panel = this.nextElementSibling
+// //         console.log(panel)
+// //     if(panel.style.maxHeight) {
+// //         panel.style.maxHeight = null
+// //     }
+
+// //     else {
+// //         panel.style.maxHeight = panel.scrollHeight + "px"
+// //         console.log(panel.scrollHeight)
+// //     }
+
+
+// //     })
+// // }
+
+// // // Fuad
+// // // 1994
+
+// // // @fuad1994
+
+// // // console.log(Number(null))
+// // // typeof null //obyekt
